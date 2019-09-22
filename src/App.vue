a863b96b3740d20a88ad2881083257e1c7e8f1c6
Arna <sky-vamp@mail.ru>
2019-09-22 17:08:25 +0300

    http request

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,107 @@
 <template>
-  <div :class="$style.container">
+  <div :class="$style.container"
+        class="pt-2">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav">
-          <!--<li class="nav-item">
-            <router-link class="nav-link" to="/">home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/cars">cars</router-link>
-          </li> -->
-          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
-            <a class="nav-link">Home</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" to="/cars" active-class="active">
-            <a class="nav-link">Cars</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" to="/car/3" active-class="active">
-            <a class="nav-link">Cars3</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" to="/car/4" active-class="active">
-            <a class="nav-link">Cars4</a>
-          </router-link>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+            <!--<li class="nav-item">
+              <router-link class="nav-link" to="/">home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/cars">cars</router-link>
+            </li> -->
+            <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+              <a class="nav-link">Home</a>
+            </router-link>
+            <router-link tag="li" class="nav-item" to="/cars" active-class="active">
+              <a class="nav-link">Cars</a>
+            </router-link>
+            <router-link tag="li" class="nav-item" to="/car/3" active-class="active">
+              <a class="nav-link">Cars3</a>
+            </router-link>
+            <router-link tag="li" class="nav-item" to="/car/4" active-class="active">
+              <a class="nav-link">Cars4</a>
+            </router-link>
 
-        </ul>
-      </div>
+          </ul>
+        </div>
     </nav>
       <router-view></router-view>
+      <div class="form-group">
+        <label for="name">
+          Name
+        </label>
+        <input id="name" class="form-control" type="text" v-model.trim="CarName">
+      </div>
+       <div class="form-group">
+        <label for="year">
+          Year
+        </label>
+        <input id="year" class="form-control" type="text" v-model.number="CarYear">
+      </div>
+
+      <button class="btn btn-success" @click="createCar">Create car</button>
+      <button class="btn btn-primary" @click="loadCars">Load Cars</button>
+
+      <hr>
+
+      <ul class="list-group">
+        <li
+          class="list-group-item"
+          v-for="car in cars"
+          :key="car.id">
+          <strong>{{ car.name }}</strong> - {{ car.year }}
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
+      CarName: '',
+      CarYear: 2018,
+      cars: [],
+      resource: null
+    }
+  },
+  methods: {
+    createCar() {
+      const car = {
+        name: this.CarName,
+        year: this.CarYear
+      }
 
+      // this.$http.post('http://localhost:3000/cars', car)
+      //   .then(response => {
+      //     return response.json()
+      //   })
+      //   .then(newCar => {
+      //     console.log(newCar)
+      //   })
+
+      this.resource.save({}, car)
+    },
+      // put delete
+    loadCars() {
+      // this.$http.get('http://localhost:3000/cars')
+      //  .then(response => {
+      //     return response.json()
+      //   })
+      //   .then(cars => {
+      //     this.cars = cars
+      //   })
+      this.resource.get()
+        .then(response => response.json())
+        .then(cars => this.cars = cars)
+    }
+  },
+
+  created() {
+    this.resource = this.$resource('cars');
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import Vuelidate from 'vuelidate'
 import VueRouter from 'vue-router'
 import App from './App.vue'
@@ -6,6 +7,15 @@ import router from './routes'
 
 Vue.use(Vuelidate)
 Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.http.options.root = 'http://localhost:3000/'
+
+Vue.http.interceptors.push(request => {
+
+  request.headers.set('Auth', 'RAND TOKEN ' + Math.random())
+
+})
 
 new Vue({
   el: '#app',
